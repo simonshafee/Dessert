df94ca91d47a407e251e2723afcf99a422438dcd
simonshafee <sayedalishafee1@gmail.com>
2022-11-15 13:03:58 +0000

    mes

--- activity14nov.js
+++ activity14nov.js
@@ -7,9 +7,22 @@ class Rental{
     }
      shopInfo(name){
         this.name= name
-        return ` ${this.name} shop is in ${this.postcode} location and his contact number is:${this.tel}`
+        return ` ${this.name} shop is in ${this.postcode} location and they contact number is:${this.tel} and list of available movies is ${this.list}.`
 
      }
 }
-const test= new Rental("test", "M1111", "07323232323","drame, classic, action")
-console.log(test.shopInfo("simon"))
+class addMovie extends Rental{
+  constructor(name,postcode,tel,list,nmovies,price){
+    super(name,postcode,tel,list);
+    this.nmovies=nmovies;
+    this.price=price
+}
+addM(name){
+  this.name=name;
+  return `${this.name} shop is in ${this.postcode} location and they contact number is:${this.tel} and list of available movies is ${this.list[0]} ${this.price[0]} ${this.list[1]} ${this.price[1]} ${this.list[2]} ${this.price[2]} and has new movies:${this.nmovies[0]} ${this.price[4]} ${this.nmovies[1]} ${this.price[4]} ${this.nmovies[2]} ${this.price[4]} ${this.nmovies[3]} ${this.price[4]} .`
+}
+}
+const test= new Rental("test", "M1111", "07323232323","drama, classic, action")
+const nem = new addMovie("test", "M1111", "07323232323","drama, classic, action","star wars, mafia, life in jungle3, spider man 12")
+const price = new addMovie("test", "M1111", "07323232323",["drama", 'classic', "action"],["star wars", 'mafia', "life in jungle3", "spider man 12"],["price:2£","price:3£","price:4£","price:5£","price:6£"])
+console.log(price.addM("desk-1"))
