03634be5c69624ca321e40559811522f3f63e1d3
simonshafee <sayedalishafee1@gmail.com>
2022-11-24 09:49:45 +0000

    asdas

--- cyberpetFFFF.js
+++ cyberpetFFFF.js
@@ -1,51 +1,122 @@
-class Pet {
-    constructor(name) {
-        this.name = name;
-        this.drinkLevel = 10;
-        this.foodLevel = 10;
-        this.happiness = 10;
-        this.health = 10;
-        
-    }
-    get addDrink() {
-        return `Drink Level:${this.drinkLevel+=5} `;
-    }
-    get addfood() {
-        return `Food Level:${this.foodLevel+=5}`
+/////////////INFO DISPLAY
+const health =document.getElementById("health");
+const hunger = document.getElementById("hunger");
+const thirst = document.getElementById("thirst");
+const cleanliness = document.getElementById("cleanliness");
+const happiness= document.getElementById("happiness");
+///////////////// BUTTONS
+const feed = document.getElementById("feed");
+const drink = document.getElementById("drink");
+const play = document.getElementById("play");
+const sleep = document.getElementById("sleep")
+const image = document.getElementById("pet-img")
+
+let pet = {
+    hunger: 50,
+    thirst: 50,
+    happiness: 50,
+    cleanliness:50,
+    health:100,
+    feedpet(){
+    /////sub
+    this.hunger -=5;
+    this.cleanliness -=5;
+    this.happiness -= 5;
+    //sum
+    this.thirst +=5;
+    this.health+=10;
+    },
+    drinkpet(){
+        ///sub
+        this.thirst-=10;
+        this.cleanliness-=5;
+        ///sum
+        this.happiness-=5;
+        this.happiness-=5;
+        this.health+=5;
+    },
+    playpet(){
+        ///sum
+        this.play+=10;
+        this.thirst+=10;
+        this.hunger+=10;
+        this.happiness-=5;
+        this.health-=5;
+        ///sub
+        this.cleanliness-=10
+    },
+    goSleep(){
+        ///sum
+        this.happiness+=10;
+        this.thirst+=10;
+        this.hunger+=10;
+        this.cleanliness+=20
     }
 
-    get play() {
-        return `happiness:${this.happiness+=5}`
-    }
-    get sleep() {
-        return `health Level:${this.health+=5}`
-    }
+}
+
+function chechCondition(){
+    if(pet.health>=100)
+    pet.health = 100;
+    if(pet.health<=0)
+    pet.health = 0;
+    if(pet.happiness>=100)
+    pet.happiness = 100;
+    if(pet.happiness<=0)
+    pet.happiness = 0;
+    if(pet.hunger>=100)
+    pet.hunger= 100;
+    if(pet.hunger<=0)
+    pet.hunger= 0;
+    if(pet.thirst<=0)
+    pet.thirst= 0;
+    if(pet.thirst>=100)
+    pet.thirst= 100;
+    if(pet.cleanliness>=100)
+    pet.cleanliness= 100;
+    if(pet.cleanliness<=0)
+    pet.cleanliness= 0;
+    if(pet.hunger<=20)
+    image.src = "./soniceat1.gif"
+    if(pet.health<=40)
+    image.src = "./sonichealth.gif"
+    if(pet.health===100&&pet.hunger===100&&pet.cleanliness===100&&pet.thirst===100&&pet.hunger===100)
+    image.src = "./sonic.gif"
     
 }
 
-let dog = new Pet("dog");
-let cat = new Pet("cat");
-
-// console.log(dog)
-// document.write("<h1>sdasdasdx<h1>")
 
 
+function infoRender(){
+    health.textContent = `health: ${pet.health}`;
+    hunger.textContent = `hunger ${pet.hunger}`
+    thirst.textContent = `thirst: ${pet.thirst}`;
+    happiness.textContent = `happiness: ${pet.happiness}`;
+    cleanliness.textContent = `cleanliness: ${pet.cleanliness}`;
+}
+
+feed.addEventListener("click", () => {
+    pet.feedpet();
+    chechCondition();
+    infoRender();
+});
+drink.addEventListener("click", () => {
+    pet.drinkpet();
+    chechCondition();
+    infoRender();
+});
+
+play.addEventListener("click",()=>{
+    pet.playpet();
+    chechCondition();
+    infoRender();
+
+});
+sleep.addEventListener("click",()=>{
+    pet.goSleep();
+    chechCondition();
+    infoRender();
+})
 
 
-
-
-//   document.getElementById("k1").onclick = dog.addDrink
-
-
-//const drink = document.dog.addDrink
-
-// console.log(dog.addfood);
-// console.log(dog.addfood);
-// console.log(dog.addfood);
-// console.log(dog.addDrink)
-// console.log(dog.addDrink)
-// console.log(dog);
-// console.log(cat.addfood)
-// console.log(cat.addfood)
-// console.log(cat.addfood)
-// console.log(cat)
+infoRender();
